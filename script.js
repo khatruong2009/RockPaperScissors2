e596c2fd1b74b6ce558b5b51f1910b29827c1e14
Kha Truong <khatruong2009@gmail.com>
2020-06-17 14:13:56 -0400

    completed the rock paper scissors 2 project

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// delcaring variables and DOM manipulations
 let ran;
 let computerSelection = computerPlay();
 let playerSelection; 
@@ -6,7 +7,26 @@ let compScore = 0;
 let rock = document.querySelector("#rock");
 let paper = document.querySelector("#paper");
 let scissors = document.querySelector("#scissors");
+let result = document.querySelector("#result");
+let pScore = document.querySelector("#pScore");
+let cScore = document.querySelector("#cScore");
+let cChoice = document.querySelector("#cChoice");
+let message = document.querySelector("#message");
+let img = document.querySelector("#img");
+let compImage = document.querySelector("#compImage");
+let reset = document.querySelector("#reset");
 
+
+//function to reset the game
+reset.addEventListener("click", () => {
+ myScore = 0;
+ compScore = 0;
+ pScore.textContent = ("Your Score: " + myScore);
+ cScore.textContent = ("Computer Score: " + compScore);
+ message.textContent = "";
+})
+
+//functions to start the game when the user clicks an image
 rock.addEventListener("click", () => {
  rps("r");
 });
@@ -18,19 +38,6 @@ paper.addEventListener("click", () => {
 scissors.addEventListener("click", () => {
  rps("s");
 });
-
-//combines below functions and loops it while keeping score
-function game() {
- while (myScore < 3 && compScore < 3) {
- console.log(rps(playerSelection));
- console.log("Your score is: " + myScore, "The computer's score is: " +  compScore);
- computerSelection = computerPlay();
- }
- if (myScore == 3) {
-  return "Game Over. You Win!";
- } else return "Game Over. You Lost.";
-}
-
 
 //function to get a random number
 function random () {
@@ -51,31 +58,70 @@ function computerPlay() {
 //function to play rock paper scissors
 function rps(playerSelection) {
  computerSelection = computerPlay();
- console.log("The computer chose " + computerSelection);
+ cChoice.textContent = ("The computer chose " + computerSelection + ".");
  if (computerSelection == "rock" && playerSelection == "r") {
-  return "It's a tie, try again.";
+  message.classList.add("message");
+  compImage.classList.add("compImage");
+  img.src = "rock.png";
+  result.textContent = "It's a tie, try again.";
  } else if (computerSelection == "rock" && playerSelection == "p") {
+  message.classList.add("message");
+  compImage.classList.add("compImage");
   myScore += 1;
-  return "You win! Paper beats Rock.";
+  img.src = "rock.png";
+  result.textContent = "You win! Paper beats Rock.";
  } else if (computerSelection == "rock" && playerSelection == "s") {
+  message.classList.add("message");
+  compImage.classList.add("compImage");
   compScore += 1;
-  return "You lose. Rock beats Scissors.";
+  img.src = "rock.png";
+  result.textContent = "You lose. Rock beats Scissors.";
  } else if (computerSelection == "scissors" && playerSelection == "r") {
+  message.classList.add("message");
+  compImage.classList.add("compImage");
+  img.src = "scissors.png";
   myScore += 1;
-  return "You win! Rock beats Scissors.";
+  result.textContent = "You win! Rock beats Scissors.";
  } else if (computerSelection == "scissors" && playerSelection == "s") {
-  return "It's a tie, try again.";
+  message.classList.add("message");
+  compImage.classList.add("compImage");
+  img.src = "scissors.png";
+  result.textContent = "It's a tie, try again.";
  } else if (computerSelection == "scissors"  && playerSelection == "p") {
+  message.classList.add("message");
+  compImage.classList.add("compImage");
   compScore += 1;
-  return "You lose. Scissors beats Paper.";
+  img.src = "scissors.png";
+  result.textContent = "You lose. Scissors beats Paper.";
  } else if (computerSelection == "paper" && playerSelection == "r") {
+  message.classList.add("message");
+  compImage.classList.add("compImage");
   compScore += 1;
-  return "You lose. Paper beats Rock";
+  img.src = "paper.png";
+  result.textContent = "You lose. Paper beats Rock.";
  } else if (computerSelection == "paper" && playerSelection == "s") {
+  message.classList.add("message");
+  compImage.classList.add("compImage");
   myScore += 1;
-  return "You win! Scissors beats Paper";
- } else return "It's a tie, try again.";
+  img.src = "paper.png";
+  result.textContent = "You win! Scissors beats Paper.";
+ } else {
+  message.classList.add("message");
+  compImage.classList.add("compImage");
+  img.src = "paper.png";
+  result.textContent = "It's a tie, try again.";
 }
 
+//function to display the score
+ pScore.textContent = ("Your Score: " + myScore);
+ cScore.textContent = ("Computer Score: " + compScore);
+
+//testing whether or not the game is over
+ if (myScore > 2) {
+  message.textContent = "You Win!"
+ } else if (compScore > 2) {
+  message.textContent = "You Lose."
+ }
+}
 
 
